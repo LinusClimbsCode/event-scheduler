b2dcccc69fb726e73ebd74d5e0c7804fd209cee0
Linus Mühlpointner <l.muehlpointner@web.de>
2025-06-21 17:39:52 +0200

    add burgermenu when no user is logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import Logo from '../assets/linuslinusLogoBK.svg';
+import burgerMenu from '../assets/burger-menu-right-svgrepo-com.svg';
 import NavbarLinks from "./NavbarLinks.jsx"
 import { Link } from "react-router"
+import { useContext } from "react"
+import { AuthContext } from '../context/AuthProvider.jsx';
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext) 
+
     return (
         <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
     <Link to='/' className='btn btn-ghost' >
-    <img src={Logo} alt="Logo" width="76" height="76" />
+      <img src={Logo} alt="Logo" width="76" height="76" />
     </Link>
   </div>
   <div className="flex-none">
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
-          <img
+          {(!user) ? (<img src={burgerMenu} alt="Logo" width="76" height="76" />) : (<img
             alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />)}
         </div>
       </div>
       <ul
