fad2ac43191f425e464188da0c8e26e5df1e21e9
Linus Mühlpointner <l.muehlpointner@web.de>
2025-06-23 13:53:07 +0200

    no events found page added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,26 @@
-import { EventCard } from '../components';
+import { Error404, EventCard, NoEvents } from '../components';
 import { useLoaderData } from 'react-router';
 
 const HomePage = () => {
   const eventData = useLoaderData();
-  const events = eventData.data.results;
+  const events = eventData?.data?.results || [];
 
-  let gridCols = "grid-cols-1";
-  if (events.length === 2) gridCols = "grid-cols-2";
-  if (events.length >= 3) gridCols = "grid-cols-1 sm:grid-cols-2 md:grid-cols-3";
+  if (events.length == 0) {
+    return <NoEvents />;
+  } else {
+    let gridCols = 'grid-cols-1';
+    if (events.length === 2) gridCols = 'grid-cols-2';
+    if (events.length >= 3)
+      gridCols = 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3';
 
-  return (
-    <div className={`grid ${gridCols} gap-8 justify-items-center mt-8`}>
-      {events.map((event) => (
-        <EventCard key={event.id} event={event} />
-      ))}
-    </div>
-  );
+    return (
+      <div className={`grid ${gridCols} gap-8 justify-items-center mt-8`}>
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default HomePage;
